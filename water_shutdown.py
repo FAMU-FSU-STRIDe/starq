048c1fff6e8133202bdad3ddfe39acc48bf2548e
Derek Vasquez <derekavasquez@gmail.com>
2024-06-26 15:07:40 -0400

    jetson shuts down when leak sensor triggered

--- water_shutdown.py
+++ water_shutdown.py
@@ -2,8 +2,34 @@ import Jetson.GPIO as GPIO
 import os
 import time
 
+# To run this in the background as a service:
+# 1. ensure the script has executable permissions. From the script location:
+# chmod +x water_shutdown.py
+# 2. add the following text to /etc/systemd/system/water_shutdown.service
+
+############################################################
+# [Unit]
+# Description=Water Leakage Shutdown Service
+# After=multi-user.target
+
+# [Service]
+# Type=simple
+# ExecStart=/usr/bin/python3 /path/to/your/water_shutdown.py
+# Restart=on-failure
+
+# [Install]
+# WantedBy=multi-user.target
+
+################################################################
+# 3. Enable and start the service
+# sudo systemctl enable water_shutdown.service
+# sudo systemctl start water_shutdown.service
+
+
+
+
 # Pin Definitions
-sensor_pin = 18  # Adjust this to the GPIO pin you're using
+sensor_pin = 15  # Adjust this to the GPIO pin you're using
 
 # Setup GPIO
 GPIO.setmode(GPIO.BOARD)
